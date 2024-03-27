5116d2b82b4fc0c5ed745cc3102a3035fbc1e5d6
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-03-27 20:01:03 +0000

    added styling

--- src/App.js
+++ src/App.js
@@ -12,7 +12,40 @@ function App() {
       <div className="description">
       <input type="text" placeholder={`description`}/>
       </div>
+      <button type={"submit"}>Add new transaction</button>
       </form>
+      <div className="transactions">
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Samsung Tv</div>
+            <div className="description">it was time for new tv</div>
+          </div>
+          <div className="right">
+            <div className="price red">-$500</div>
+            <div className="datetime">2024-03-27 19:00</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">Gig job new website</div>
+            <div className="description">it was time for new tv</div>
+          </div>
+          <div className="right ">
+            <div className="price green">+$400</div>
+            <div className="datetime">2024-03-27 19:00</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">iPhone</div>
+            <div className="description">it was time for new tv</div>
+          </div>
+          <div className="right">
+            <div className="price red">-$900</div>
+            <div className="datetime">2024-03-27 19:00</div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
