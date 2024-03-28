db83dbb03625477daf601e89e83bdc89ad374164
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-03-28 20:33:23 +0000

    updated import express

--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
-const express = require("express");
-const { Response, NextFunction } = require("express");
+import express from "express";
 const app = express();
 
 app.get("/api/test", (req, res) => {
-  res.json({ body: "test ok" }); // Corrected the syntax for response JSON
+  res.json({ body: "test ok" });
 });
 
-const port = 4040; // Corrected the syntax for defining port
+const port = 4040;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
